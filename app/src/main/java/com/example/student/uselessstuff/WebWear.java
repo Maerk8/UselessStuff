97a48a50477e4eabb2bca0d422acb4219c10fd8c
RyanH <ryanhall364yt2@gmail.com>
2016-03-15 13:58:14 -0500

    ♥

--- app/src/main/java/com/example/student/uselessstuff/WebWear.java
+++ app/src/main/java/com/example/student/uselessstuff/WebWear.java
@@ -4,10 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.SeekBar;
+
+import java.util.Objects;
 
 public class WebWear extends AppCompatActivity {
 
     public static WebView wvWebMain;
+
+    public static SeekBar sbUseless;
+    public static SeekBar sbUseful;
+    public static SeekBar sbRandom;
+
+    public static int Usefulness;
+    public static int Uselessness;
+    public static int Randomness;
+    public static String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +28,25 @@ public class WebWear extends AppCompatActivity {
 
         wvWebMain = (WebView) findViewById(R.id.vwWebMain);
 
+        sbUseless = (SeekBar) findViewById(R.id.sbUseless);
+        sbUseful = (SeekBar) findViewById(R.id.sbUseful);
+        sbRandom = (SeekBar) findViewById(R.id.sbRandom);
+
+
+        //WebView set settings/creation of pages
         WebSettings webSettings = wvWebMain.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        wvWebMain.loadUrl("http://haneke.net/");
+        wvWebMain.loadUrl("http://www.everydayim.com/");
+
+        Object[] values = new Object[50];
+
+        values[1] = new String("test");
+        values[1] = new Integer(Usefulness);
+        System.out.print(values[1]);
+
+    }
+    public void SeekingMovement(SeekBar sbUseless, SeekBar sbUseful, SeekBar sbRandom,
+                                Integer usefulness, Integer uselessness, Integer randomness) {
 
     }
 }
